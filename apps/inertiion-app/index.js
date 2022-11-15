97ae9c3dcb0ff91d0cf9400b051767590b3b9e63
thankyouthatscool <85291297+thankyouthatscool@users.noreply.github.com>
2022-11-15 18:20:12 +1000

    Initial setup for expo to make Metro work with a monorepo.

---
+++ apps/inertiion-app/index.js
@@ -0,0 +1,8 @@
+import { registerRootComponent } from "expo";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
+
+import { App } from "./App";
+
+const AppHOCWrapper = gestureHandlerRootHOC(() => <App />);
+
+registerRootComponent(AppHOCWrapper);
